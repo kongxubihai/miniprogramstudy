48b1f101d0e732a392c7db10acbcae92a7c9e40e
kongxubihai <1769699920@qq.com>
2019-06-10 18:16:27 +0800

    初始化

---
+++ xpyun-mp/src/components/click-counter/click-counter.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="click-counter">
+    <div class="counter-num">次数: {{num}}</div>
+    <button class="counter-btn" @click="handleIncreaseClick">点我加1</button>
+    <button class="counter-reset-btn" @click="handleResetClick">点我清零</button>
+    <slot></slot>
+  </div>  
+</template>
+
+<script>
+export default {
+  // 增加一个可从外部传入的属性initNum
+  props: {
+    initNum: {
+      type: Number,
+      default: 0
+    }
+  },
+
+  data () {
+    return {
+      num: this.initNum
+    }
+  },
+
+  methods: {
+    handleIncreaseClick () {
+      this.num += 1
+      this.notifyNum()
+    },
+    handleResetClick () {
+      this.num = 0
+      this.notifyNum()
+    },
+    notifyNum () {
+      this.$emit('clicknum', {
+        num: this.num
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.click-counter {
+  display: flex;
+  align-items: center;
+  border: 1px red solid;
+  padding: 10px;
+  background-color: #ffffff;
+}
+
+.counter-num {
+  margin: 3px;
+  flex: 1
+}
+.counter-btn {
+  margin: 3px;
+  flex: 1
+}
+.counter-reset-btn {
+  margin: 3px;
+  flex: 1
+}
+
+</style>
---
+++ xpyun-mp/src/pages/index/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="container" >
+    <div>计数结果: {{count}}</div>
+    <a href="/pages/test/main" class="navlink">进入计数页面</a>
+    <div class="message" @click="clickHandle">{{msg}}</div>
+    <!-- 使用3个 click-counter 组件 -->
+    <click-counter :init-num='10' @clicknum="handleClickNum" /> 
+    <click-counter :init-num='20' @clicknum="handleClickNum" /> 
+    <click-counter :init-num='30' @clicknum="handleClickNum" >
+        <!-- 填坑用... -->
+    <input type="checkbox" /> 禁用
+    </click-counter>
+  </div>
+</template>
+<script>
+import ClickCounter from '@/components/click-counter/click-counter'
+import golbalStore from '../../stores/global-store'
+
+export default {
+  // 声明在当前组件下使用 counter-click 组件
+  components: { ClickCounter },
+  data () {
+    return {
+      msg: 'Hello from zfd!'
+    }
+  },
+  methods: {
+    clickHandle () {
+      this.msg = 'Click!!!!'
+    },
+    handleClickNum (data) {
+      console.log('>>>>>>>>>', data.num)
+    }
+  },
+  computed: {
+    count () {
+      return golbalStore.state.count
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .message {
+      color: red;
+      padding: 10px;
+      text-align: center;
+  }
+  .navlink {
+    text-decoration: underline;
+  }
+</style>
+
